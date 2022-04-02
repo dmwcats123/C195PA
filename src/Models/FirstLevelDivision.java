7db84512210010e5d42045c14cedcf7639c11d62
dmwcats123 <dmwcats123@gmail.com>
2022-04-01 20:13:54 -0700

    implemented DAO interface and FLD model class

---
+++ src/Models/FirstLevelDivision.java
@@ -0,0 +1,78 @@
+package Models;
+
+public class FirstLevelDivision {
+    private int divisionID;
+    private String division;
+    private String createDate;
+    private String createdBy;
+    private String lastUpdate;
+    private String lastUpdatedBy;
+    private int countryID;
+
+    FirstLevelDivision() {}
+    FirstLevelDivision(int divisionID, String division, String createDate, String createdBy, String lastUpdate,
+                       String lastUpdatedBy, int countryID) {
+        this.divisionID =divisionID;
+        this.division = division;
+        this.createDate = createDate;
+        this.createdBy = createdBy;
+        this.lastUpdate = lastUpdate;
+        this.lastUpdatedBy = lastUpdatedBy;
+        this.countryID = countryID;
+    }
+    public int getDivisionID() {
+        return divisionID;
+    }
+
+    public String getCreateDate() {
+        return createDate;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public String getDivision() {
+        return division;
+    }
+
+    public String getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public int getCountryID() {
+        return countryID;
+    }
+
+    public String getLastUpdatedBy() {
+        return lastUpdatedBy;
+    }
+
+    public void setCreateDate(String createDate) {
+        this.createDate = createDate;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public void setDivision(String division) {
+        this.division = division;
+    }
+
+    public void setDivisionID(int divisionID) {
+        this.divisionID = divisionID;
+    }
+
+    public void setLastUpdate(String lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public void setCountryID(int countryID) {
+        this.countryID = countryID;
+    }
+
+    public void setLastUpdatedBy(String lastUpdatedBy) {
+        this.lastUpdatedBy = lastUpdatedBy;
+    }
+}
