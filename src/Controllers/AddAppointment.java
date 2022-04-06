558937a896c84980517d54632045e9f9791f4175
dmwcats123 <dmwcats123@gmail.com>
2022-04-06 18:08:34 -0500

    refactored addappointment to allow user to type time, rather than selecting from combobox.

--- src/Controllers/AddAppointment.java
+++ src/Controllers/AddAppointment.java
@@ -4,15 +4,17 @@ import DAO.AppointmentDao;
 import DAO.ContactDAO;
 import Helpers.TimeUtility;
 import Models.Appointment;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Optional;
 
 public class AddAppointment {
     @FXML TextField titleField;
@@ -22,37 +24,14 @@ public class AddAppointment {
     @FXML ComboBox<String> contactCombo;
     @FXML TextField typeField;
     @FXML DatePicker date;
-    @FXML ComboBox<String> startTime;
-    @FXML ComboBox<String> endTime;
+    @FXML TextField startTime;
+    @FXML TextField endTime;
     @FXML TextField customerID;
     @FXML TextField userID;
 
     @FXML
     public void initialize() throws Exception {
-        populateTimeCombos();
         populateContactCombo();
-    }
-
-    public void populateTimeCombos() {
-        ObservableList<String> easternTimes = FXCollections.observableArrayList();
-        ObservableList<String> localTimes = FXCollections.observableArrayList();
-        String[] quarterHours = {"00","15","30","45"};
-        for (int i = 8; i < 22; i++) {
-            for (int j = 0; j < 4; j++) {
-                String time = "";
-                if (i < 10) {
-                    time = "1999-01-01 0" + i + ":" + quarterHours[j] + ":00";
-                } else {
-                    time = "1999-01-01 " + i + ":" + quarterHours[j] + ":00";
-                }
-                easternTimes.add(time);
-            }
-        }
-        for(String easternTime: easternTimes) {
-            localTimes.add(TimeUtility.estToLocalTime(easternTime));
-        }
-        startTime.setItems(localTimes);
-        endTime.setItems(localTimes);
     }
 
     public void populateContactCombo() throws Exception {
@@ -60,24 +39,40 @@ public class AddAppointment {
     }
 
     public void submitButtonClicked() throws Exception {
-        Appointment appointment = new Appointment();
-        appointment.setTitle(titleField.getText());
-        appointment.setDescription(descriptionField.getText());
-        appointment.setLocation(locationField.getText());
-        appointment.setType(typeField.getText());
-        String start = TimeUtility.localToUTCTime(date.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")) + " "
-                + startTime.getSelectionModel().getSelectedItem(), DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-        appointment.setStart(start);
-        String end = TimeUtility.localToUTCTime(date.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")) +
-                " " + endTime.getSelectionModel().getSelectedItem(), DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-        appointment.setEnd(end);
-        appointment.setCustomerID(Integer.valueOf(customerID.getText()));
-        appointment.setUserID(Integer.valueOf(userID.getText()));
-        appointment.setContactID(ContactDAO.get(contactCombo.getSelectionModel().getSelectedItem()).getContactID());
-        appointment.setCreatedBy(Main.currentUser.getUsername());
-        appointment.setLastUpdatedBy(Main.currentUser.getUsername());
-        appointment.setCreateDate(TimeUtility.localToUTCTime(LocalDateTime.now().toString()));
-        appointment.setLastUpdate(TimeUtility.localToUTCTime(LocalDateTime.now().toString()));
-        AppointmentDao.add(appointment);
+        try {
+            Appointment appointment = new Appointment();
+            appointment.setTitle(titleField.getText());
+            appointment.setDescription(descriptionField.getText());
+            appointment.setLocation(locationField.getText());
+            appointment.setType(typeField.getText());
+            String start = TimeUtility.localToUTCTime(date.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")) + " "
+                    + startTime.getText(), DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+            appointment.setStart(start);
+            String end = TimeUtility.localToUTCTime(date.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")) +
+                    " " + endTime.getText(), DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+            appointment.setEnd(end);
+            appointment.setCustomerID(Integer.valueOf(customerID.getText()));
+            appointment.setUserID(Integer.valueOf(userID.getText()));
+            appointment.setContactID(ContactDAO.get(contactCombo.getSelectionModel().getSelectedItem()).getContactID());
+            appointment.setCreatedBy(Main.currentUser.getUsername());
+            appointment.setLastUpdatedBy(Main.currentUser.getUsername());
+            appointment.setCreateDate(TimeUtility.localToUTCTime(LocalDateTime.now().toString()));
+            appointment.setLastUpdate(TimeUtility.localToUTCTime(LocalDateTime.now().toString()));
+            AppointmentDao.add(appointment);
+            Stage currentStage= (Stage) startTime.getScene().getWindow();
+            currentStage.close();
+
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../Views/ManageAppointments.fxml"));
+            Parent root1 = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.show();
+
+        } catch(DateTimeParseException e) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setHeaderText("DateTime Exception!");
+            alert.setContentText("Start and end times must be in format HH:mm");
+            Optional<ButtonType> info = alert.showAndWait();
+        }
     }
 }
