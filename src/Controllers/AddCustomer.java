b2f6896e9b92cb1258454d427755edc2cf86aa83
dmat104 <dmat104@wgu.edu>
2022-09-17 16:29:30 -0600

    Added Lambdas

--- src/Controllers/AddCustomer.java
+++ src/Controllers/AddCustomer.java
@@ -11,6 +11,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -28,36 +29,62 @@ public class AddCustomer {
     @FXML TextField postalCodeField;
     @FXML TextField phoneNumberField;
     @FXML ComboBox<String> fldCombo;
+    @FXML Button submitButton;
     public void initialize() {
         fillCountryComboBox();
+        countryCombo.valueProperty().addListener((obs, oldVal, newVal) -> {
+            ObservableList<String> fld = FXCollections.observableArrayList();
+            try {
+                Connection connection = Database.makeConnection();
+                PreparedStatement statement =  connection.prepareStatement("SELECT * FROM countries WHERE Country = ?");
+                statement.setObject(1, countryCombo.getValue());
+                ResultSet result = statement.executeQuery();
+                result.next();
+                String country_ID = result.getString("Country_ID");
+                PreparedStatement divisionStatement =  connection.prepareStatement("SELECT Division FROM first_level_divisions WHERE Country_ID = ?");
+                divisionStatement.setObject(1, country_ID);
+                ResultSet divisionResult = divisionStatement.executeQuery();
+                divisionResult = divisionStatement.executeQuery();
+                while (divisionResult.next()) {
+                    fld.add(divisionResult.getString("Division"));
+                }
+                fldCombo.setItems(fld);
+            } catch (Exception e) {
+                System.out.println("Exception " + e);
+            }
+        });
+
+        submitButton.setOnAction(actionEvent -> {
+            try {
+                Customer newCustomer = new Customer();
+                newCustomer.setCustomerName(nameField.getText());
+                newCustomer.setCustomerAddress(addressField.getText());
+                newCustomer.setCustomerPostalCode(postalCodeField.getText());
+                newCustomer.setCustomerPhone(phoneNumberField.getText());
+                newCustomer.setCreateDate(TimeUtility.localToUTCTime(LocalDateTime.now().toString()));
+                newCustomer.setCreatedBy(Main.currentUser.getUsername());
+                newCustomer.setLastUpdate(TimeUtility.localToUTCTime(LocalDateTime.now().toString()));
+                newCustomer.setLastUpdatedBy(Main.currentUser.getUsername());
+                newCustomer.setDivisionID(FldDAO.get(fldCombo.getSelectionModel().getSelectedItem()).getDivisionID());
+                CustomerDao.add(newCustomer);
+                Stage currentStage= (Stage) fldCombo.getScene().getWindow();
+                currentStage.close();
+
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../Views/ManageCustomers.fxml"));
+                Parent root1 = fxmlLoader.load();
+                Stage stage = new Stage();
+                stage.setScene(new Scene(root1));
+                stage.show();
+            } catch(Exception e) {
+
+            }
+    });
     }
 
     @FXML public void submitButtonClicked() throws Exception {
-        Customer newCustomer = new Customer();
-        newCustomer.setCustomerName(nameField.getText());
-        newCustomer.setCustomerAddress(addressField.getText());
-        newCustomer.setCustomerPostalCode(postalCodeField.getText());
-        newCustomer.setCustomerPhone(phoneNumberField.getText());
-        newCustomer.setCreateDate(TimeUtility.localToUTCTime(LocalDateTime.now().toString()));
-        newCustomer.setCreatedBy(Main.currentUser.getUsername());
-        newCustomer.setLastUpdate(TimeUtility.localToUTCTime(LocalDateTime.now().toString()));
-        newCustomer.setLastUpdatedBy(Main.currentUser.getUsername());
-        newCustomer.setDivisionID(FldDAO.get(fldCombo.getSelectionModel().getSelectedItem()).getDivisionID());
-        CustomerDao.add(newCustomer);
-        Stage currentStage= (Stage) fldCombo.getScene().getWindow();
-        currentStage.close();
-
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../Views/ManageCustomers.fxml"));
-        Parent root1 = fxmlLoader.load();
-        Stage stage = new Stage();
-        stage.setScene(new Scene(root1));
-        stage.show();
 
     }
 
-    @FXML public void countryComboClicked() {
-        fillFldComboBox();
-    }
 
     public void fillCountryComboBox() {
         ObservableList<String> countries = FXCollections.observableArrayList();
@@ -73,31 +100,6 @@ public class AddCustomer {
         } catch (Exception e) {
             System.out.println("Exception " + e);
         }
-
-    }
-
-    public void fillFldComboBox() {
-        ObservableList<String> fld = FXCollections.observableArrayList();
-
-        try {
-            Connection connection = Database.makeConnection();
-            PreparedStatement statement =  connection.prepareStatement("SELECT * FROM countries WHERE Country = ?");
-            statement.setObject(1, countryCombo.getValue());
-            ResultSet result = statement.executeQuery();
-            result.next();
-            String country_ID = result.getString("Country_ID");
-            PreparedStatement divisionStatement =  connection.prepareStatement("SELECT Division FROM first_level_divisions WHERE Country_ID = ?");
-            divisionStatement.setObject(1, country_ID);
-            ResultSet divisionResult = divisionStatement.executeQuery();
-            divisionResult = divisionStatement.executeQuery();
-            while (divisionResult.next()) {
-                fld.add(divisionResult.getString("Division"));
-            }
-            fldCombo.setItems(fld);
-        } catch (Exception e) {
-            System.out.println("Exception " + e);
-        }
-
 
     }
 }
